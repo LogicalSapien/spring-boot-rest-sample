9348dd8dfeaca45c983dfad2b1979cccb95719a1
Elmo Yeldo <elmoyeldo@gmail.com>
2019-10-24 13:20:14 +0100

    import ordering corrected

--- src/main/java/com/logicalsapien/springbootrestsample/controller/PlanetController.java
+++ src/main/java/com/logicalsapien/springbootrestsample/controller/PlanetController.java
@@ -1,11 +1,11 @@
 package com.logicalsapien.springbootrestsample.controller;
 
-import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 
 
